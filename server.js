b9b1deb11ea298b76b9820053464bc839fc020df
champagne.lyb <champagne.lyb@gmail.com>
2017-02-28 20:33:13 +0800

    jshint is shit

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ pageApp.use(express.static(path.join(__dirname, 'page')));
 app.use(bodyParser.text());
 // var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By", "lyb");
-    res.header("Content-Type", "application/json;charset=utf-8");
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+    res.header('X-Powered-By', 'lyb');
+    res.header('Content-Type', 'application/json;charset=utf-8');
     next();
 });
 
